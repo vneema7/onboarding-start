161ab9d7b076c7fad66868ca5f0cf38a6130f499
vneema7 <vedant.neema2007@gmail.com>
2026-03-25 22:17:25 -0400

    Implement SPI register writes

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -6,6 +6,7 @@ module spi_peripheral (
     input  wire       sclk,
     input  wire       mosi,
     input  wire       ncs,
+
     output reg  [7:0] en_reg_out_7_0,
     output reg  [7:0] en_reg_out_15_8,
     output reg  [7:0] en_reg_pwm_7_0,
@@ -14,7 +15,7 @@ module spi_peripheral (
 );
 
     reg sclk_ff1, sclk_ff2;
-    reg ncs_ff1, ncs_ff2;
+    reg ncs_ff1,  ncs_ff2;
     reg mosi_ff1, mosi_ff2;
 
     reg [15:0] shift_reg;
@@ -30,19 +31,19 @@ module spi_peripheral (
         if (!rst_n) begin
             sclk_ff1 <= 1'b0;
             sclk_ff2 <= 1'b0;
-            ncs_ff1 <= 1'b1;
-            ncs_ff2 <= 1'b1;
+            ncs_ff1  <= 1'b1;
+            ncs_ff2  <= 1'b1;
             mosi_ff1 <= 1'b0;
             mosi_ff2 <= 1'b0;
 
             shift_reg <= 16'h0000;
             bit_count <= 5'd0;
 
-            en_reg_out_7_0 <= 8'h00;
+            en_reg_out_7_0  <= 8'h00;
             en_reg_out_15_8 <= 8'h00;
-            en_reg_pwm_7_0 <= 8'h00;
+            en_reg_pwm_7_0  <= 8'h00;
             en_reg_pwm_15_8 <= 8'h00;
-            pwm_duty_cycle <= 8'h00;
+            pwm_duty_cycle  <= 8'h00;
         end else begin
             sclk_ff2 <= sclk_ff1;
             sclk_ff1 <= sclk;
@@ -62,11 +63,11 @@ module spi_peripheral (
                 if (bit_count == 5'd15) begin
                     if (shift_reg[14] == 1'b1) begin
                         case (shift_reg[13:7])
-                            7'h00: en_reg_out_7_0 <= {shift_reg[6:0], mosi_ff2};
+                            7'h00: en_reg_out_7_0  <= {shift_reg[6:0], mosi_ff2};
                             7'h01: en_reg_out_15_8 <= {shift_reg[6:0], mosi_ff2};
-                            7'h02: en_reg_pwm_7_0 <= {shift_reg[6:0], mosi_ff2};
+                            7'h02: en_reg_pwm_7_0  <= {shift_reg[6:0], mosi_ff2};
                             7'h03: en_reg_pwm_15_8 <= {shift_reg[6:0], mosi_ff2};
-                            7'h04: pwm_duty_cycle <= {shift_reg[6:0], mosi_ff2};
+                            7'h04: pwm_duty_cycle  <= {shift_reg[6:0], mosi_ff2};
                             default: begin end
                         endcase
                     end
