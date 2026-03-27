9b414b3735a0947c61dfc728900109e9863f2e72
vneema7 <vedant.neema2007@gmail.com>
2026-03-26 20:11:58 -0400

    spi port mismatch

--- src/project.v
+++ src/project.v
@@ -23,12 +23,12 @@ module tt_um_uwasic_onboarding_vedant_neema (
   wire [7:0] en_reg_pwm_7_0;
   wire [7:0] en_reg_pwm_15_8;
   wire [7:0] pwm_duty_cycle;
-  spi_peripheral spi_peripheral_inst (
+  spi_peripheral spi_inst (
     .clk(clk),
     .rst_n(rst_n),
-    .COPI(ui_in[1]),
-    .nCS(ui_in[2]),
-    .SCLK(ui_in[0]),
+    .sclk(ui_in[0]),
+    .mosi(ui_in[1]),
+    .ncs(ui_in[2]),
     .en_reg_out_7_0(en_reg_out_7_0),
     .en_reg_out_15_8(en_reg_out_15_8),
     .en_reg_pwm_7_0(en_reg_pwm_7_0),
--- src/pwm_peripheral.v
+++ src/pwm_peripheral.v
@@ -1,7 +1,3 @@
-/*
- * Copyright (c) 2024 Damir Gazizullin
- * SPDX-License-Identifier: Apache-2.0
- */
 
 /*
  * Copyright (c) 2024 Damir Gazizullin
@@ -11,55 +7,55 @@
 `default_nettype none
 
 module pwm_peripheral (
-    input  wire       clk,
-    input  wire       rst_n,
+    input  wire       clk,      // clock
+    input  wire       rst_n,     // reset_n - low to reset
     input  wire [7:0] en_reg_out_7_0,
     input  wire [7:0] en_reg_out_15_8,
     input  wire [7:0] en_reg_pwm_7_0,
     input  wire [7:0] en_reg_pwm_15_8,
     input  wire [7:0] pwm_duty_cycle,
-    output wire [15:0] out
+    output reg [15:0] out
 );
 
-    // 10 MHz / (13 * 256) = 3004.8 Hz
-    localparam [3:0] CLK_DIV_TRIG = 4'd12;
-
-    reg [3:0] clk_counter;
+    localparam clk_div_trig = 12; // Divide by (12+1)*256, yielding 3000 (3004.80769) Hz
+    reg [10:0] clk_counter;
     reg [7:0] pwm_counter;
+    wire pwm_signal = (pwm_duty_cycle == 8'hFF) ? 1'b1 : (pwm_counter < pwm_duty_cycle); // 253 is 98.82% 254 is 99.21%, 255 is 100%, not 99.61%
 
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            clk_counter <= 4'd0;
-            pwm_counter <= 8'd0;
+            out <= 0;
+            pwm_counter <= 0;
+            clk_counter <= 0;
         end else begin
-            if (clk_counter == CLK_DIV_TRIG) begin
-                clk_counter <= 4'd0;
-                pwm_counter <= pwm_counter + 8'd1;
-            end else begin
-                clk_counter <= clk_counter + 4'd1;
+            clk_counter <= clk_counter + 1;
+            if (clk_counter == clk_div_trig) begin
+                pwm_counter <= pwm_counter + 1;
+                clk_counter <= 0;
             end
+            out[7:0] <= en_reg_out_7_0;
+            out[15:8] <= en_reg_out_15_8;
+            // Apply PWM to each bit individually if enabled
+            // Lower 8 bits
+            if (en_reg_pwm_7_0[0]) out[0] <= (pwm_signal) ? en_reg_out_7_0[0] : 1'b0;
+            if (en_reg_pwm_7_0[1]) out[1] <= (pwm_signal) ? en_reg_out_7_0[1] : 1'b0;
+            if (en_reg_pwm_7_0[2]) out[2] <= (pwm_signal) ? en_reg_out_7_0[2] : 1'b0;
+            if (en_reg_pwm_7_0[3]) out[3] <= (pwm_signal) ? en_reg_out_7_0[3] : 1'b0;
+            if (en_reg_pwm_7_0[4]) out[4] <= (pwm_signal) ? en_reg_out_7_0[4] : 1'b0;
+            if (en_reg_pwm_7_0[5]) out[5] <= (pwm_signal) ? en_reg_out_7_0[5] : 1'b0;
+            if (en_reg_pwm_7_0[6]) out[6] <= (pwm_signal) ? en_reg_out_7_0[6] : 1'b0;
+            if (en_reg_pwm_7_0[7]) out[7] <= (pwm_signal) ? en_reg_out_7_0[7] : 1'b0;
+
+            // Upper 8 bits
+            if (en_reg_pwm_15_8[0]) out[8] <= (pwm_signal) ? en_reg_out_15_8[0] : 1'b0;
+            if (en_reg_pwm_15_8[1]) out[9] <= (pwm_signal) ? en_reg_out_15_8[1] : 1'b0;
+            if (en_reg_pwm_15_8[2]) out[10] <= (pwm_signal) ? en_reg_out_15_8[2] : 1'b0;
+            if (en_reg_pwm_15_8[3]) out[11] <= (pwm_signal) ? en_reg_out_15_8[3] : 1'b0;
+            if (en_reg_pwm_15_8[4]) out[12] <= (pwm_signal) ? en_reg_out_15_8[4] : 1'b0;
+            if (en_reg_pwm_15_8[5]) out[13] <= (pwm_signal) ? en_reg_out_15_8[5] : 1'b0;
+            if (en_reg_pwm_15_8[6]) out[14] <= (pwm_signal) ? en_reg_out_15_8[6] : 1'b0;
+            if (en_reg_pwm_15_8[7]) out[15] <= (pwm_signal) ? en_reg_out_15_8[7] : 1'b0;
         end
     end
-
-    wire pwm_signal;
-    assign pwm_signal = (pwm_duty_cycle == 8'hFF) ? 1'b1 : (pwm_counter < pwm_duty_cycle);
-
-    assign out[0]  = en_reg_out_7_0[0]  ? (en_reg_pwm_7_0[0]  ? pwm_signal : 1'b1) : 1'b0;
-    assign out[1]  = en_reg_out_7_0[1]  ? (en_reg_pwm_7_0[1]  ? pwm_signal : 1'b1) : 1'b0;
-    assign out[2]  = en_reg_out_7_0[2]  ? (en_reg_pwm_7_0[2]  ? pwm_signal : 1'b1) : 1'b0;
-    assign out[3]  = en_reg_out_7_0[3]  ? (en_reg_pwm_7_0[3]  ? pwm_signal : 1'b1) : 1'b0;
-    assign out[4]  = en_reg_out_7_0[4]  ? (en_reg_pwm_7_0[4]  ? pwm_signal : 1'b1) : 1'b0;
-    assign out[5]  = en_reg_out_7_0[5]  ? (en_reg_pwm_7_0[5]  ? pwm_signal : 1'b1) : 1'b0;
-    assign out[6]  = en_reg_out_7_0[6]  ? (en_reg_pwm_7_0[6]  ? pwm_signal : 1'b1) : 1'b0;
-    assign out[7]  = en_reg_out_7_0[7]  ? (en_reg_pwm_7_0[7]  ? pwm_signal : 1'b1) : 1'b0;
-
-    assign out[8]  = en_reg_out_15_8[0] ? (en_reg_pwm_15_8[0] ? pwm_signal : 1'b1) : 1'b0;
-    assign out[9]  = en_reg_out_15_8[1] ? (en_reg_pwm_15_8[1] ? pwm_signal : 1'b1) : 1'b0;
-    assign out[10] = en_reg_out_15_8[2] ? (en_reg_pwm_15_8[2] ? pwm_signal : 1'b1) : 1'b0;
-    assign out[11] = en_reg_out_15_8[3] ? (en_reg_pwm_15_8[3] ? pwm_signal : 1'b1) : 1'b0;
-    assign out[12] = en_reg_out_15_8[4] ? (en_reg_pwm_15_8[4] ? pwm_signal : 1'b1) : 1'b0;
-    assign out[13] = en_reg_out_15_8[5] ? (en_reg_pwm_15_8[5] ? pwm_signal : 1'b1) : 1'b0;
-    assign out[14] = en_reg_out_15_8[6] ? (en_reg_pwm_15_8[6] ? pwm_signal : 1'b1) : 1'b0;
-    assign out[15] = en_reg_out_15_8[7] ? (en_reg_pwm_15_8[7] ? pwm_signal : 1'b1) : 1'b0;
 
 endmodule
