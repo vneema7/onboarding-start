0f1b483fd8e9d5457630473d048c45b9d9e2f215
vneema7 <vedant.neema2007@gmail.com>
2026-03-26 20:05:13 -0400

    fixed project.v

--- src/project.v
+++ src/project.v
@@ -1,53 +1,52 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Vedant Neema
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
 module tt_um_uwasic_onboarding_vedant_neema (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path
-    input  wire       ena,      // always 1 when the design is powered
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+  input  wire [7:0] ui_in,    // Dedicated inputs
+  output wire [7:0] uo_out,   // Dedicated outputs
+  input  wire [7:0] uio_in,   // IOs: Input path
+  output wire [7:0] uio_out,  // IOs: Output path
+  output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+  input  wire       ena,
+  input  wire       clk,
+  input  wire       rst_n
 );
 
-    assign uio_oe = 8'hFF;
+  assign uio_oe = 8'hFF;
 
-    wire [7:0] en_reg_out_7_0;
-    wire [7:0] en_reg_out_15_8;
-    wire [7:0] en_reg_pwm_7_0;
-    wire [7:0] en_reg_pwm_15_8;
-    wire [7:0] pwm_duty_cycle;
+  wire [7:0] en_reg_out_7_0;
+  wire [7:0] en_reg_out_15_8;
+  wire [7:0] en_reg_pwm_7_0;
+  wire [7:0] en_reg_pwm_15_8;
+  wire [7:0] pwm_duty_cycle;
+  spi_peripheral spi_peripheral_inst (
+    .clk(clk),
+    .rst_n(rst_n),
+    .COPI(ui_in[1]),
+    .nCS(ui_in[2]),
+    .SCLK(ui_in[0]),
+    .en_reg_out_7_0(en_reg_out_7_0),
+    .en_reg_out_15_8(en_reg_out_15_8),
+    .en_reg_pwm_7_0(en_reg_pwm_7_0),
+    .en_reg_pwm_15_8(en_reg_pwm_15_8),
+    .pwm_duty_cycle(pwm_duty_cycle)
+);
 
-    spi_peripheral spi_inst (
-        .clk(clk),
-        .rst_n(rst_n),
-        .sclk(ui_in[0]),
-        .mosi(ui_in[1]),
-        .ncs(ui_in[2]),
-        .en_reg_out_7_0(en_reg_out_7_0),
-        .en_reg_out_15_8(en_reg_out_15_8),
-        .en_reg_pwm_7_0(en_reg_pwm_7_0),
-        .en_reg_pwm_15_8(en_reg_pwm_15_8),
-        .pwm_duty_cycle(pwm_duty_cycle)
-    );
+  pwm_peripheral pwm_peripheral_inst (
+    .clk(clk),
+    .rst_n(rst_n),
+    .en_reg_out_7_0(en_reg_out_7_0),
+    .en_reg_out_15_8(en_reg_out_15_8),
+    .en_reg_pwm_7_0(en_reg_pwm_7_0),
+    .en_reg_pwm_15_8(en_reg_pwm_15_8),
+    .pwm_duty_cycle(pwm_duty_cycle),
+    .out({uio_out, uo_out})
+  );
 
-    pwm_peripheral pwm_peripheral_inst (
-        .clk(clk),
-        .rst_n(rst_n),
-        .en_reg_out_7_0(en_reg_out_7_0),
-        .en_reg_out_15_8(en_reg_out_15_8),
-        .en_reg_pwm_7_0(en_reg_pwm_7_0),
-        .en_reg_pwm_15_8(en_reg_pwm_15_8),
-        .pwm_duty_cycle(pwm_duty_cycle),
-        .out({uio_out, uo_out})
-    );
-
-    wire _unused = &{ena, ui_in[7:3], uio_in, 1'b0};
+  wire _unused = &{ena, ui_in[7:3], uio_in, 1'b0};
 
 endmodule
